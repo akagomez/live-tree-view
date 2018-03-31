5ef0879e40faaa717448877cbc00f69c01c93ad9
Chris Gomez <akagomez@gmail.com>
2018-03-31 01:59:02 -0400

    Remove unnecessary comments

--- react-ui/src/index.js
+++ react-ui/src/index.js
@@ -11,26 +11,19 @@ var socket = new WebSocket(`${protocol}://${window.location.host}/ws`);
 // TODO: Remove this once the interface is wired up
 window.socket = socket;
 
-// Handle any errors that occur.
 socket.onerror = function(error) {
   console.log('WebSocket Error: ' + error);
 };
 
-
-// Show a connected message when the WebSocket is opened.
 socket.onopen = function(event) {
   console.log('onopen: ' + event.currentTarget.url);
 };
 
-
-// Handle messages sent by the server.
 socket.onmessage = function(event) {
   var message = event.data;
   console.log('onmessage:' + message);
 };
 
-
-// Show a disconnected message when the WebSocket is closed.
 socket.onclose = function(event) {
   console.log('onclose');
 };
