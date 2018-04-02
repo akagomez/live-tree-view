144e534f559375dd70157b7cf6420310501f83de
Chris Gomez <akagomez@gmail.com>
2018-04-02 07:14:25 -0400

    Add gzip compression; Add comments to the server setup

--- server/index.js
+++ server/index.js
@@ -1,51 +1,60 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const compression = require('compression')
 const morgan = require('morgan')
 const path = require('path');
-
 const { errors } = require('celebrate');
-
 const mongoose = require('mongoose');
-
-const url = require('url');
 const WebSocket = require('ws');
-
-const PORT = process.env.PORT || 5000;
 
 const bootstrapResources = require('./resources');
 const createDispatcher = require('./dispatcher');
 
+const PORT = process.env.PORT || 5000;
+
 const app = express();
 
-app.use(morgan('tiny'))
+// Enable gzip compression
+app.use(compression())
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI);
-
-const db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log('Connected to db!')
-});
-
-// Priority serve any static files.
+// Resolve static files
 app.use(express.static(path.resolve(__dirname, '../react-ui/build')));
 
 // Parse JSON payloads
 app.use(bodyParser.json());
 
+// Log HTTP requests
+app.use(morgan('tiny'))
+
+// Start the HTTP server
 const server = app.listen(PORT, function () {
-  console.error(`Node cluster worker ${process.pid}: listening on port ${PORT}`);
+  console.error(`Listening on port ${PORT}`);
 });
 
+// Monitor the database connection
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+  console.log(`Connected to ${process.env.MONGODB_URI}`)
+});
+
+// Initialize the database connection
+mongoose.connect(process.env.MONGODB_URI)
+
+// Add support for WebSockets to the existing server
 const wss = new WebSocket.Server({ server });
 
+// Coordinate client WebSocket subscriptions
 dispatcher = createDispatcher(wss)
+
+// Create the necessary HTTP endpoints
 bootstrapResources(app, dispatcher)
 
+// Handle all validation errors
 app.use(errors());
 
-// All remaining requests return the React app, so it can handle routing.
+// Send unresolved requests to the single-page application
+// to handle routing client-side
 app.get('*', function(request, response) {
   response.sendFile(path.resolve(__dirname, '../react-ui/build', 'index.html'));
 });
